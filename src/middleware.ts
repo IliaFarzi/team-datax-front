ea1d9dac16ce2170ce653de44756ba40214c8d94
Amirgolli <168966152+Amirgolli@users.noreply.github.com>
2025-09-07 14:09:55 +0330

    redirect to log in whith add middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const publicPaths = [
+  "/login",
+  "/signup",
+  "/forgetPassword",
+  "/checkEmail",
+  "/resetPassword",
+  "/not-found",
+  "/api/auth",
+];
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (publicPaths.some((path) => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get("access_token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
