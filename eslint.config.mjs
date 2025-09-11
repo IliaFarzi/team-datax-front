1975567ad8adcb0a6b5d2d615fd8f508473b2f73
Amirgolli <168966152+Amirgolli@users.noreply.github.com>
2025-09-11 13:01:03 +0330

    chnage config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
