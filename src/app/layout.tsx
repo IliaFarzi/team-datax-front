b14b44db60b24634b4ddb4f4b6ee439ac5211309
Amirgolli <168966152+Amirgolli@users.noreply.github.com>
2025-09-20 23:29:12 +0330

    change back-end url to domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,13 +33,13 @@ export const metadata: Metadata = {
     type: "website",
     siteName: "DataX",
     locale: "fa_IR",
-    url: "https://datax-ai.vercel.app",
+    url: "https://dataxai.ir/",
     title: "DataX – چت‌بات هوش مصنوعی برای آنالیز داده‌ها",
     description:
       "با DataX داده‌های خود را با کمک هوش مصنوعی تحلیل کنید و تصمیم‌های هوشمندانه‌تری بگیرید.",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/images/logo.png",
         width: 1200,
         height: 630,
         alt: "DataX AI Chatbot Preview",
@@ -48,44 +48,22 @@ export const metadata: Metadata = {
   },
   authors: [
     {
-      name: "DataX Team",
-      url: "https://datax-ai.vercel.app",
+      name: "Datax Team",
+      url: "https://dataxai.ir/",
     },
   ],
-  creator: "DataX Team",
+  creator: "Datax Team",
   icons: [
-    { rel: "icon", url: "/favicon.ico" },
-    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
     {
       rel: "icon",
       type: "image/png",
-      url: "/favicon-32x32.png",
-      sizes: "32x32",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon-16x16.png",
-      sizes: "16x16",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-192x192.png",
-      sizes: "192x192",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-512x512.png",
-      sizes: "512x512",
+      url: "/images/logo.png",
     },
   ],
   robots: {
     index: true,
     follow: true,
   },
-  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
